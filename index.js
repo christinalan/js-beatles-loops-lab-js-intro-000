89b5e096d90c7e97e3bed6bf719dbd48bf649e83
Christina Lan <christina.lan13@gmail.com>
2019-02-13 14:53:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ function johnLennonFacts() {
   }
   return array;
 }
+
+function iLoveTheBeatles(num) {
+  var love [];
+  do {
+    love.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+
+  return love;
+}
